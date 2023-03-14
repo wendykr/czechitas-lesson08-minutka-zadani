4e1021d4c6c369da1e9dfdc6e65aaae2b2716c85
Vendula Krajíčková <vendysacek@seznam.cz>
2023-03-14 05:21:20 +0100

    Hotový úkol

---
+++ README.md
@@ -0,0 +1,14 @@
+# Funkce vyšších řádů, události - Lekce 8
+
+Domácí úkol pro kurz JavaScript 1 od Czechitas.
+
+## 3. Minutka
+
+Naklonujte si [repozitář](https://github.com/Czechitas-podklady-WEB/minutka-zadani) pro stránku s kuchyňskou minutkou. Využijte funkci `setTimeout` ke splnění následujících úkolů.
+
+- Zařiďte, aby minutka začala zvonit za pět vteřin po otevření stránky. Zvonění zařídíte tak, že k elementu budíku přidáte CSS třídu `alarm--ring`.
+- Nechte uživatele skrze `prompt` zadat kolik vteřin má uplynout než minutka začne zvonit.
+- Můžete zařídit i spuštění zvuku. Stačí ze stránky vybrat `audio` element a zavolat na něm metodu `play`. Abyste zvuk slyšeli, musíte po zadání času na stránku kliknout. Prohlížeč Chrome totiž brání stránkám přehrávat audio či video dokud uživatel se stránkou nějak neinteragoval.
+- Přidejte na stránku tlačítko, které umožní odpočet minutky zrušit dřív, než začne zvonit.
+
+![ukázka výsledku](https://kodim.cz/cms/assets/kurzy/javascript-vyuka/js-1/hof-udalosti/ulozky-na-doma/cvdoma%3Eminutka/ukazka.gif)
--- index.js
+++ index.js
@@ -1,1 +1,83 @@
 console.log('funguju!');
+
+/*
+const alarmRing = () => {
+    let alarmElm = document.querySelector('.alarm');
+    alarmElm.classList.add('alarm--ring');
+    console.log('5 vteřin uběhlo, zvoní');
+}
+
+//setTimeout(alarmRing, 5000);
+*/
+
+let buttonElm = document.querySelectorAll('button');
+
+const proces = () => {
+    let vstupUzivatel = Number(prompt("Za jak dlouho má minutka zvonit?"));
+    let seconds = vstupUzivatel * 1000;
+    console.log(seconds);
+
+    buttonElm[1].classList.remove('disabled');
+    
+    const alarmRing = () => {
+        let alarmElm = document.querySelector('.alarm');
+        alarmElm.classList.add('alarm--ring');
+    }
+
+    let resetAlarmRing = setTimeout(alarmRing, seconds);
+
+    let audioElm = document.querySelector('audio');
+
+    const spustAudio = () => {
+        audioElm.play();
+    }
+
+    let vypniAudio = setTimeout(spustAudio, seconds);
+
+    const pauseAudio = () => {
+        audioElm.pause();
+    }
+
+    buttonElm[1].addEventListener('click', pauseAudio);
+
+    const resetRing = () => {
+        let alarmElm = document.querySelector('.alarm');
+        alarmElm.classList.remove('alarm--ring');
+    }
+
+    buttonElm[1].addEventListener('click', resetRing);
+
+    let infoboxElm = document.querySelector('.infobox');
+
+    const odecitej = () => {
+
+        if (vstupUzivatel === 2) {
+            --vstupUzivatel;
+            infoboxElm.textContent = 'Minutka bude zvonit za ' + vstupUzivatel + ' sekundu';
+        } else if (vstupUzivatel >= 3 && vstupUzivatel <= 5) {
+            --vstupUzivatel;
+            infoboxElm.textContent = 'Minutka bude zvonit za ' + vstupUzivatel + ' sekundy';
+        } else if (vstupUzivatel >= 6) {
+            --vstupUzivatel;
+            infoboxElm.textContent = 'Minutka bude zvonit za ' + vstupUzivatel + ' sekund';
+        } else {
+            infoboxElm.textContent = 'Zvoní';
+        }
+        console.log(vstupUzivatel);
+    }
+
+    let resetOdecitej = setInterval(odecitej, 1000);
+
+    const resetFunction = () => {
+        clearInterval(resetOdecitej);
+        clearTimeout(resetAlarmRing);
+        clearTimeout(vypniAudio);
+        pauseAudio();
+        infoboxElm.textContent = 'Vypnuto';
+        buttonElm[1].classList.add('disabled');
+    }
+
+    buttonElm[1].addEventListener('click', resetFunction);
+}
+
+buttonElm[0].addEventListener('click', proces);
