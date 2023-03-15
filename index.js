30bb9643a656deec8611fe87674ee0a4baf2f31d
Vendula Krajíčková <vendysacek@seznam.cz>
2023-03-15 20:00:36 +0100

    add input as number

--- index.js
+++ index.js
@@ -14,70 +14,79 @@ let buttonElm = document.querySelectorAll('button');
 
 const proces = () => {
     let vstupUzivatel = Number(prompt("Za jak dlouho má minutka zvonit?"));
-    let seconds = vstupUzivatel * 1000;
-    console.log(seconds);
 
-    buttonElm[1].classList.remove('disabled');
-    
-    const alarmRing = () => {
-        let alarmElm = document.querySelector('.alarm');
-        alarmElm.classList.add('alarm--ring');
-    }
+    if (vstupUzivatel === 0 || vstupUzivatel === '' || !containsOnlyNumbers(vstupUzivatel)) {
+        alert('Neplatný vstup');
+    } else {
+        let seconds = vstupUzivatel * 1000;
+        console.log(seconds);
 
-    let resetAlarmRing = setTimeout(alarmRing, seconds);
-
-    let audioElm = document.querySelector('audio');
-
-    const spustAudio = () => {
-        audioElm.play();
-    }
-
-    let vypniAudio = setTimeout(spustAudio, seconds);
-
-    const pauseAudio = () => {
-        audioElm.pause();
-    }
-
-    buttonElm[1].addEventListener('click', pauseAudio);
-
-    const resetRing = () => {
-        let alarmElm = document.querySelector('.alarm');
-        alarmElm.classList.remove('alarm--ring');
-    }
-
-    buttonElm[1].addEventListener('click', resetRing);
-
-    let infoboxElm = document.querySelector('.infobox');
-
-    const odecitej = () => {
-
-        if (vstupUzivatel === 2) {
-            --vstupUzivatel;
-            infoboxElm.textContent = 'Minutka bude zvonit za ' + vstupUzivatel + ' sekundu';
-        } else if (vstupUzivatel >= 3 && vstupUzivatel <= 5) {
-            --vstupUzivatel;
-            infoboxElm.textContent = 'Minutka bude zvonit za ' + vstupUzivatel + ' sekundy';
-        } else if (vstupUzivatel >= 6) {
-            --vstupUzivatel;
-            infoboxElm.textContent = 'Minutka bude zvonit za ' + vstupUzivatel + ' sekund';
-        } else {
-            infoboxElm.textContent = 'Zvoní';
+        buttonElm[1].classList.remove('disabled');
+        
+        const alarmRing = () => {
+            let alarmElm = document.querySelector('.alarm');
+            alarmElm.classList.add('alarm--ring');
         }
-        console.log(vstupUzivatel);
+
+        let resetAlarmRing = setTimeout(alarmRing, seconds);
+
+        let audioElm = document.querySelector('audio');
+
+        const spustAudio = () => {
+            audioElm.play();
+        }
+
+        let vypniAudio = setTimeout(spustAudio, seconds);
+
+        const pauseAudio = () => {
+            audioElm.pause();
+        }
+
+        buttonElm[1].addEventListener('click', pauseAudio);
+
+        const resetRing = () => {
+            let alarmElm = document.querySelector('.alarm');
+            alarmElm.classList.remove('alarm--ring');
+        }
+
+        buttonElm[1].addEventListener('click', resetRing);
+
+        let infoboxElm = document.querySelector('.infobox');
+
+        const odecitej = () => {
+
+            if (vstupUzivatel === 2) {
+                --vstupUzivatel;
+                infoboxElm.textContent = 'Minutka bude zvonit za ' + vstupUzivatel + ' sekundu';
+            } else if (vstupUzivatel >= 3 && vstupUzivatel <= 5) {
+                --vstupUzivatel;
+                infoboxElm.textContent = 'Minutka bude zvonit za ' + vstupUzivatel + ' sekundy';
+            } else if (vstupUzivatel >= 6) {
+                --vstupUzivatel;
+                infoboxElm.textContent = 'Minutka bude zvonit za ' + vstupUzivatel + ' sekund';
+            } else {
+                infoboxElm.textContent = 'Zvoní';
+            }
+            console.log(vstupUzivatel);
+        }
+
+        let resetOdecitej = setInterval(odecitej, 1000);
+
+        const resetFunction = () => {
+            clearInterval(resetOdecitej);
+            clearTimeout(resetAlarmRing);
+            clearTimeout(vypniAudio);
+            pauseAudio();
+            infoboxElm.textContent = 'Vypnuto';
+            buttonElm[1].classList.add('disabled');
+        }
+
+        buttonElm[1].addEventListener('click', resetFunction);
     }
+}
 
-    let resetOdecitej = setInterval(odecitej, 1000);
-
-    const resetFunction = () => {
-        clearInterval(resetOdecitej);
-        clearTimeout(resetAlarmRing);
-        clearTimeout(vypniAudio);
-        pauseAudio();
-        infoboxElm.textContent = 'Vypnuto';
-        buttonElm[1].classList.add('disabled');
-    }
-
-    buttonElm[1].addEventListener('click', resetFunction);
+function containsOnlyNumbers(str) {
+    return /^\d+$/.test(str);
 }
 
 buttonElm[0].addEventListener('click', proces);
